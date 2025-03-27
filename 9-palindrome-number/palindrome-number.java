d40ecc0ac0754a808c124d4ad715d979cad4d2c5
AJDazzle <110547826+AJDazzle@users.noreply.github.com>
2025-03-27 11:08:20 +0530

    Time: 19 ms (5.71%) | Memory: 45 MB (5.39%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false; // Negative numbers can't be palindromes
-        String a = Integer.toString(x);
-        
-        int left = 0;
-        int right = a.length() - 1;
-        
-        // Compare characters from both ends
-        while (left < right) {
-            if (a.charAt(left) != a.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        String a =Integer.toString(x);
+        String b="";
+        for(int i=a.length()-1;i>=0;i--)
+        {
+            b+=a.charAt(i);
         }
-        
-        return true;
+        return a.equals(b);
     }
 }
