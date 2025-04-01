c7f26d51bbadbb63f2b4ee83e486841e55e77cd6
AJDazzle <110547826+AJDazzle@users.noreply.github.com>
2025-04-01 19:05:18 +0530

    Time: 1 ms (29.84%) | Memory: 42.2 MB (60.27%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,16 +1,8 @@
 import java.util.*;
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-     int a=0;
-     for(int i=m;i<nums1.length;i++)
-     {
-        nums1[i]=nums2[a];
-        a++;
-     }
+
+        System.arraycopy(nums2,0,nums1,m,n);
         Arrays.sort(nums1);
-        for(int i=0;i<nums1.length;i++)
-        {
-            System.out.print(nums1[i]);
-        } 
     }
 }
